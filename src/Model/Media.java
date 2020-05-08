d6104c91211b815f7a5a3c714c5101dcda01716b
psjuliano <psouzajuliano@hotmail.com>
2020-05-08 19:19:42 +0100

    FirstVersion

---
+++ src/Model/Media.java
@@ -0,0 +1,94 @@
+
+package Model;
+
+/**
+ *
+ * @author Priscila
+ */
+  public class Media {
+    // ***** Attributes *****
+    protected String idMedia;
+    protected String title;
+    protected int yearOfRelease;
+    protected float price;
+    protected int rentedDays;
+    protected int availability;
+    protected MediaFormat mediaFormat;
+    protected String description;
+    
+    @Override
+    public String toString() {
+        return "Media{" + "idMedia=" + idMedia + ", title=" + title + ", yearOfRelease=" +
+         yearOfRelease + ", price=" + price + ", rentedDays=" + rentedDays + ", availability=" + 
+           availability + ", mediaFormat=" + mediaFormat + ", description=" + description + '}';
+    }
+            
+    
+    public String getIdMedia() {
+        return idMedia;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+    
+    public float getPrice() {
+        return price;
+    }
+   
+    public int getRentedDays() {
+        return rentedDays;
+    }
+
+    public int getAvailability() {
+        return availability;
+    }
+   
+    public String getMediaFormat() {
+        return mediaFormat.toString().toUpperCase();
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setIdMedia(String idMedia) {
+        this.idMedia = idMedia.valueOf(idMedia);
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYearOfRelease(int yearOfRelease) {
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setRentedDays(int rentedDays) {
+        this.rentedDays = rentedDays;
+    }
+
+    public void setAvailability(int availability) {
+        this.availability = availability;
+    }
+
+    public void setMediaFormat(String mediaFormat) {
+        this.mediaFormat = MediaFormat.valueOf(mediaFormat.toUpperCase());
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+   
+    
+}
---
+++ src/Model/PlanType.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Priscila
+ */
+public class PlanType {
+    protected int idPlan; 
+    protected String planName;
+
+    public int getIdPlan() {
+        return idPlan;
+    }
+
+    public void setIdPlan(int idPlan) {
+        this.idPlan = idPlan;
+    }
+
+    public String getPlanName() {
+        return planName;
+    }
+
+    public void setPlanName(String planName) {
+        this.planName = planName;
+    }
+   
+
+   
+    
+}
